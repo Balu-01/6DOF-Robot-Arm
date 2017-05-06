4e434caeff4c9761483f00ce9e69f4f0d8537162
tutRPi <tutRPi@users.noreply.github.com>
2017-05-06 22:15:23 +0200

    Update Robot.py

--- Robot.py
+++ Robot.py
@@ -1,5 +1,5 @@
 from components import Report, Servo, Hand
-
+import time
 
 class Robot:
     def __init__(self):
@@ -12,3 +12,15 @@ class Robot:
         }
 
         self.hand = Hand(config)
+
+
+    def sleep(self):
+        self.hand.close()
+        self.servo.move_servo_to_percent(0,50)
+        self.servo.move_servo_to_percent(1,30)
+        self.servo.move_servo_to_percent(2,0)
+        self.servo.move_servo_to_percent(3,35)
+        self.servo.move_servo_to_percent(4,50)
+        for i in range(5):
+            self.servo.sleep(i)
+            time.sleep(1)
